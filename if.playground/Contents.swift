1e221628c4ee310a2a232cf3005bfe94a9854c3b
師岡誠太 <3624@ezweb.ne.jp>
2015-09-08 11:11:57 +0900

    新規登録

---
+++ if.playground/Contents.swift
@@ -0,0 +1,19 @@
+var a = 1
+var b = 2
+if a == b {
+    println("一緒じゃない")
+}else if a < b {
+    println("bの方が大きい")
+}else {
+    println("aの方が大きい")
+}
+
+var c = 10
+var d = 20
+if c > d {
+    println("cのほうが大きい")
+}else if c < d {
+    println("dのほうが大きい")
+}else{
+    println("大きさは等しい")
+}
